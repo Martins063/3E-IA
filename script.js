c78d315461e6468de855448b19ee9f071a550db1
Martins063 <146957856+Martins063@users.noreply.github.com>
2024-08-02 09:27:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const caixaprincipal = document.querySelector('.caixa-principal');
+const caixapergunta = document.querySelector('.caixa-pergunta');
+const caixaalternativa = document.querySelector('.caixa-alternativa');
+const caixaresultado = document.querySelector('.caixa-resultado');
+
+const pergunta = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado:"pergunta 1",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+
+
+        ]
+
+    }
+]
+
